1287aa92df979bdcfa178bf9e8e5fa1fb0eedcd5
tarunsandeepraju <142690413+tarunsandeepraju@users.noreply.github.com>
2024-12-29 09:56:49 +0530

    Second Commit

--- app.py
+++ app.py
@@ -15,29 +15,31 @@ redis_client = redis.StrictRedis(
 # Configure Kafka
 kafka_admin_client = KafkaAdminClient(bootstrap_servers='localhost:9092')
 
+
 @app.route('/start', methods=['POST'])
 def start_services():
     try:
-        print("Starting services...")  # Debug
+        print("Starting services...")
         # Start Microservice 1
-        subprocess.Popen(['python', 'E:\\Finvedic Hackathon\\MarketDataSimulator\\app.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
+        subprocess.Popen(['python', 'E:\Finvedic Hackathon\MarketDataSimulator/app.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
         # Start Microservice 2
-        subprocess.Popen(['python', 'E:\\Finvedic Hackathon\\DataConsumer\\app.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
+        subprocess.Popen(['python', 'E:\Finvedic Hackathon\DataConsumer/app.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
         # Start Microservice 3
-        subprocess.Popen(['python', 'E:\\Finvedic Hackathon\\DataConsumer\\consumer.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
-        print("Microservices started")  # Debug
+        subprocess.Popen(['python', 'E:\Finvedic Hackathon\DataConsumer/consumer.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
+        print("Microservices started")
         return jsonify({"message": "Microservices started successfully!"}), 200
     except Exception as e:
-        print(f"Error starting services: {e}")  # Debug
+        print(f"Error starting services: {e}")
         return jsonify({"error": str(e)}), 500
+
+
 
 @app.route('/stop', methods=['POST'])
 def stop_services():
     try:
-        print("Stopping services...")  # Debug
-        # Stop all running microservices using taskkill for Windows
+        print("Stopping services...")
         subprocess.call(['taskkill', '/IM', 'python.exe', '/F'])
-        return "Microservices has been stopped"
+        return "Microservices stopped  successfully!"
     except Exception as e:
         print(f"Error stopping services: {e}")  # Debug
         return jsonify({"error": str(e)}), 500
